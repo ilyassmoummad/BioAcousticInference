daf92d691b1591ba56bcd30c415794fe8c5538bb
Nicolas Farrugia <nicofarr09@gmail.com>
2024-01-06 13:00:44 +0100

    added torch.nograd and eval mode for query

--- inference.py
+++ inference.py
@@ -221,79 +221,80 @@ if __name__ == "__main__":
 
     labels_pred = []
     labels_comb = []
+    with torch.no_grad():
+        encoder.train()
 
-    encoder.train()
+        pos_proto = []
+        pos_feat = torch.zeros(0, emb_dim)
+        print("Generating positive features...")
+        for b_idx, x_p in tqdm(enumerate(loader_pos),total=len(loader_pos)):
+            if args.multiview:
+                zp_views = []
+                for i_v in range(args.nviews):
+                    xp = x_p[0].to(args.device)
+                    if i_v != 0:
+                        xp = makeview(xp)
+                    zp, _ = encoder(xp)
+                    zp = zp.detach().cpu()
+                    zp_views.append(zp)
+                zp_views = torch.stack(zp_views)
+                zp_mean = zp_views.mean(0).mean(0).unsqueeze(0)
+                pos_feat = torch.cat((pos_feat, zp_mean), dim=0)
+            else:  
+                z_pos, _ = encoder(x_p[0].to(args.device))
+                z_pos = z_pos.detach().cpu()
+                z_pos_mean = z_pos.mean(dim=0).unsqueeze(0)
+                pos_feat = torch.cat((pos_feat, z_pos_mean), dim=0)
+        pos_proto = pos_feat.mean(dim=0)
 
-    pos_proto = []
-    pos_feat = torch.zeros(0, emb_dim)
-    print("Generating positive features...")
-    for b_idx, x_p in tqdm(enumerate(loader_pos),total=len(loader_pos)):
-        if args.multiview:
-            zp_views = []
-            for i_v in range(args.nviews):
-                xp = x_p[0].to(args.device)
-                if i_v != 0:
-                    xp = makeview(xp)
-                zp, _ = encoder(xp)
-                zp = zp.detach().cpu()
-                zp_views.append(zp)
-            zp_views = torch.stack(zp_views)
-            zp_mean = zp_views.mean(0).mean(0).unsqueeze(0)
-            pos_feat = torch.cat((pos_feat, zp_mean), dim=0)
-        else:  
-            z_pos, _ = encoder(x_p[0].to(args.device))
-            z_pos = z_pos.detach().cpu()
-            z_pos_mean = z_pos.mean(dim=0).unsqueeze(0)
-            pos_feat = torch.cat((pos_feat, z_pos_mean), dim=0)
-    pos_proto = pos_feat.mean(dim=0)
+        neg_proto = []
+        neg_feat = torch.zeros(0, emb_dim)
+        print("Generating negative features...")
+        for b_idx, x_n in tqdm(enumerate(loader_neg),total=len(loader_neg)):
+            if args.multiview:
+                zn_views = []
+                z_feat = torch.zeros(0, emb_dim)
+                for i_v in range(args.nviews):
+                    xn = x_n[0].to(args.device)
+                    if i_v != 0:
+                        xn = makeview(xn)
+                    zn, _ = encoder(xn)
+                    zn = zn.detach().cpu()
+                    zn_views.append(zn)
+                zn_views = torch.stack(zn_views)
+                zn_mean = zn_views.mean(0).mean(0).unsqueeze(0)
+                neg_feat = torch.cat([neg_feat, zn_mean], dim=0)            
+            else:  
+                z_neg, _ = encoder(x_n[0].to(args.device))
+                z_neg = z_neg.detach().cpu()
+                z_neg_mean = z_neg.mean(dim=0).unsqueeze(0)
+                neg_feat = torch.cat((neg_feat, z_neg_mean), dim=0)
+        neg_proto = neg_feat.mean(dim=0)
 
-    neg_proto = []
-    neg_feat = torch.zeros(0, emb_dim)
-    print("Generating negative features...")
-    for b_idx, x_n in tqdm(enumerate(loader_neg),total=len(loader_neg)):
-        if args.multiview:
-            zn_views = []
-            z_feat = torch.zeros(0, emb_dim)
-            for i_v in range(args.nviews):
-                xn = x_n[0].to(args.device)
-                if i_v != 0:
-                    xn = makeview(xn)
-                zn, _ = encoder(xn)
-                zn = zn.detach().cpu()
-                zn_views.append(zn)
-            zn_views = torch.stack(zn_views)
-            zn_mean = zn_views.mean(0).mean(0).unsqueeze(0)
-            neg_feat = torch.cat([neg_feat, zn_mean], dim=0)            
-        else:  
-            z_neg, _ = encoder(x_n[0].to(args.device))
-            z_neg = z_neg.detach().cpu()
-            z_neg_mean = z_neg.mean(dim=0).unsqueeze(0)
-            neg_feat = torch.cat((neg_feat, z_neg_mean), dim=0)
-    neg_proto = neg_feat.mean(dim=0)
+        encoder.eval()
+        labels_pred = []
+        labels_comb = []
+        print("Generating Query features...")
+        for x_q in tqdm(loader_q, total=len(loader_q)):
+            if args.multiview:
+                zq_views = []
+                for i_v in range(args.nviews):
+                    xq = x_q[0].to(args.device)
+                    if i_v != 0:
+                        xq = makeview(xq)
+                    zq, _ = encoder(xq)
+                    zq = zq.detach().cpu()
+                    zq_views.append(zq)
+                zq_views = torch.stack(zq_views)
+                z_q = zq_views.mean(0)
+            else:
+                z_q, _ = encoder(x_q[0].to(args.device))
+                z_q = z_q.detach().cpu()
 
-    labels_pred = []
-    labels_comb = []
-    print("Generating Query features...")
-    for x_q in tqdm(loader_q, total=len(loader_q)):
-        if args.multiview:
-            zq_views = []
-            for i_v in range(args.nviews):
-                xq = x_q[0].to(args.device)
-                if i_v != 0:
-                    xq = makeview(xq)
-                zq, _ = encoder(xq)
-                zq = zq.detach().cpu()
-                zq_views.append(zq)
-            zq_views = torch.stack(zq_views)
-            z_q = zq_views.mean(0)
-        else:
-            z_q, _ = encoder(x_q[0].to(args.device))
-            z_q = z_q.detach().cpu()
+            distances = get_distance(pos_proto, neg_proto, z_q)
+            label_pred = torch.argmax(distances, dim=-1)
 
-        distances = get_distance(pos_proto, neg_proto, z_q)
-        label_pred = torch.argmax(distances, dim=-1)
-
-        labels_pred.extend(label_pred.tolist())
+            labels_pred.extend(label_pred.tolist())
 
     print("Generating final predictions...")
     labels_comb.append(labels_pred)
